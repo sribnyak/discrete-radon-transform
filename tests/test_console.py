adf2dc4377f2e635deb272d3173c4b65e9c35719
Alexander Sribnyak <alexandersribnyak@gmail.com>
2024-05-16 02:46:46 +0300

    add pytest to project

---
+++ tests/test_console.py
@@ -0,0 +1,9 @@
+import click.testing
+
+from dradon import console
+
+
+def test_main_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(console.main, ['examples/geometry_test.png'])
+    assert result.exit_code == 0
