25a7291b953ad3cba9e45bdffa2d32a4ad579f2c
Alexander Sribnyak <alexandersribnyak@gmail.com>
2024-05-16 04:20:41 +0300

    add flake8 linting, update readme

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,14 @@
 import nox
 
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.9"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
 
 @nox.session(python=["3.9"])
 def tests(session):
