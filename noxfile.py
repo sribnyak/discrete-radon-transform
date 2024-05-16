5a5f68136928e9885c2a335808f88905c0e21c90
Alexander Sribnyak <alexandersribnyak@gmail.com>
2024-05-16 15:16:40 +0300

    use constrains in noxfile

--- noxfile.py
+++ noxfile.py
@@ -7,17 +7,31 @@ nox.options.sessions = "lint", "tests", "safety"
 locations = "src", "tests", "./noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python="3.9")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python="3.9")
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -29,12 +43,15 @@ def lint(session):
 
 @nox.session(python="3.9")
 def tests(session):
-    args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
     session.run("pytest", *args)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.9")
 def safety(session):
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
@@ -46,5 +63,5 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
