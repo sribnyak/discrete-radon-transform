cc7cacdbf3859778116bd3249c5d098e5cb87039
Alexander Sribnyak <alexandersribnyak@gmail.com>
2024-05-16 14:59:33 +0300

    add safety, little_test.png

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,9 @@
+import tempfile
+
 import nox
 
-nox.options.sessions = "lint", "tests"
+
+nox.options.sessions = "lint", "tests", "safety"
 locations = "src", "tests", "./noxfile.py"
 
 
@@ -29,3 +32,19 @@ def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
