9df41612e367f6826e320dc56d16eb4e72e82c84
Alexander Sribnyak <alexandersribnyak@gmail.com>
2024-05-16 15:45:13 +0300

    add local flake8 to pre-commit hooks

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -3,8 +3,8 @@ import cv2
 from dradon import dradon, draw_lines, get_lines_from_radon_image
 
 
-image = cv2.imread('geometry_test.png', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("geometry_test.png", cv2.IMREAD_GRAYSCALE)
 radon_image, shift_step = dradon(image)
-cv2.imwrite('radon_transform.png', radon_image * 255)
+cv2.imwrite("radon_transform.png", radon_image * 255)
 lines = get_lines_from_radon_image(radon_image, shift_step)
-cv2.imwrite('detected_lines.png', draw_lines(image, lines))
+cv2.imwrite("detected_lines.png", draw_lines(image, lines))
